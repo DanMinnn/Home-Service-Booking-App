12f9cad6eba5f8c0f97766e04e23184b4852936e
Dang Minh <minhdangngoc304@gmail.com>
2025-05-13 16:04:58 +0700

    style(appColors): add some colors

--- lib/themes/app_colors.dart
+++ lib/themes/app_colors.dart
@@ -11,9 +11,14 @@ class AppColors {
   static const Color subTitle = Color(0xFF3D4976);
   static const Color redMedium = Color(0xFFEA4C2D);
 
+  //error color light
   static const Color iconClose = Color(0xFFc91912);
   static const Color bubbles = Color(0xFFc81a12);
   static const Color snackBarError = Color(0xFFf64b3c);
+
+  //error color dark
+  static const Color bubblesDark = Color(0xFF7E1534);
+  static const Color snackBarErrorDark = Color(0xFFC82C41);
 }
 
 // class Gradients {
