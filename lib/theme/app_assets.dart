aca6ac169f5f0cf50ae065494b80d260aee5ba4d
Dang Minh <minhdangngoc304@gmail.com>
2025-05-24 15:33:16 +0700

    style(icons): add icons

--- lib/theme/app_assets.dart
+++ lib/theme/app_assets.dart
@@ -10,4 +10,15 @@ class AppAssetsIcons {
   static const String googleIc = '${iconPath}google_ic.png';
   static const String arrowLeft = '${iconPath}arrow_left_ic.png';
   static const String logoAppBar = '${iconPath}logo_app_bar.png';
+
+  //main screen
+  static const String homeIc = '${iconPath}home_outline_ic.png';
+  static const String homeIcActive = '${iconPath}home_filled_ic.png';
+  static const String emailIc = '${iconPath}email_outline_ic.png';
+  static const String emailActive = '${iconPath}email_filled_ic.png';
+  static const String profileIc = '${iconPath}profile_outline_ic.png';
+  static const String profileActive = '${iconPath}profile_filled_ic.png';
+  static const String menuIc = '${iconPath}menu_ic.png';
+  static const String notificationIc = '${iconPath}notification_ic.png';
+  static const String raiseHand = '${iconPath}raise_hand.png';
 }
